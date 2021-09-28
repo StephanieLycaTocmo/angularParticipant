b59b0100593e05c009ae3ccb059804e15150a5dd
unknown <lycatocmo57@gmail.com>
2021-09-28 23:35:57 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,12 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Stephanie Lyca Tocmo", age:18, gender: "Female"},
+            {name: "Ronemae Gacera", age:25, gender: "Female"},
+            {name: "Christine Gacera", age:22, gender: "Female"},
+            {name: "Alvin Jose Orcullo", age:24, gender: "Male"},
+            {name: "Carla Socrates", age:16, gender: "Male"}
+        ];
+    }
+
+}
